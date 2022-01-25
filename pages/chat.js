570366756a9aff3fcf4285d6843e37c412a5a465
Alexandre Dantas Almeida da Silva <70717962+Alex-Andrer@users.noreply.github.com>
2022-01-25 15:50:54 -0300

    first commit

---
+++ pages/chat.js
@@ -0,0 +1,7 @@
+export default function PaginaDoChat(){
+    return(
+        <div>
+            Chat
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,11 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import react from 'react';
+import {useRouter}from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
+
 function Titulo (props){
-    console.log(props)
+    //console.log(props)
     const Tag = props.tag || 'h1';
     return (
         <> 
@@ -60,11 +35,13 @@ function Titulo (props){
   //export default HomePage
 
   export default function PaginaInicial() {
-    const username = 'peas';
+    //const username = 'peas';
+    const [username, setUsername]= react.useState('');
+    const roteamento = useRouter();
   
     return (
       <>
-        <GlobalStyle />
+        
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +68,12 @@ function Titulo (props){
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infosDoEvento){
+                //console.log('Alguém entrou');
+                infosDoEvento.preventDefault();
+                roteamento.push('/chat');
+
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -102,6 +85,12 @@ function Titulo (props){
               </Text>
   
               <TextField
+              value={username}
+              onChange={function (event){
+                const valor= event.target.value;
+                console.log('Deu certo',event.target.value);
+                setUsername(valor);
+              }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
